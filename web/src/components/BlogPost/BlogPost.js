dc178adc6e319c81589b9ea827447a8788bdbc0e
Ascen Salmerón <ascensalmanez@gmail.com>
2021-09-22 18:55:16 +0200

    Create BlogPost page

---
+++ web/src/components/BlogPost/BlogPost.js
@@ -0,0 +1,14 @@
+const BlogPost = ({ post }) => {
+  return (
+    <article>
+      <header>
+        <h2>
+          <Link to={routes.blogPost({ id: post.id })}>{post.title}</Link>
+        </h2>
+      </header>
+      <div>{post.body}</div>
+    </article>
+  )
+}
+
+export default BlogPost
